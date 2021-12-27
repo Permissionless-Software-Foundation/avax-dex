80e6c275b3b4299d5c37c15646012cbf6bf0b295
Chris Troutner <chris.troutner@gmail.com>
2021-12-27 10:59:16 -0800

    Adding comments to prepare for next set of tasks

--- src/use-cases/offer.js
+++ src/use-cases/offer.js
@@ -1,156 +1,164 @@
-const { wlogger } = require('../adapters/wlogger')
+const { wlogger } = require("../adapters/wlogger");
 
-const OfferEntity = require('../entities/offer')
+const OfferEntity = require("../entities/offer");
 
 class OfferLib {
-  constructor (localConfig = {}) {
+  constructor(localConfig = {}) {
     // console.log('User localConfig: ', localConfig)
-    this.adapters = localConfig.adapters
+    this.adapters = localConfig.adapters;
     if (!this.adapters) {
       throw new Error(
-        'Instance of adapters must be passed in when instantiating Offer Use Cases library.'
-      )
+        "Instance of adapters must be passed in when instantiating Offer Use Cases library."
+      );
     }
 
     // Encapsulate dependencies
-    this.offerEntity = new OfferEntity()
-    this.OfferModel = this.adapters.localdb.Offer
-    this.bch = this.adapters.bch
+    this.offerEntity = new OfferEntity();
+    this.OfferModel = this.adapters.localdb.Offer;
+    this.bch = this.adapters.bch;
   }
 
   // Create a new offer model and add it to the Mongo database.
-  async createOffer (entryObj) {
+  async createOffer(entryObj) {
     try {
-      // Input Validation
-      const offerEntity = this.offerEntity.validate(entryObj)
+      console.log(
+        `createOffer() entryObj: ${JSON.stringify(entryObj, null, 2)}`
+      );
 
-      // Ensure sufficient tokens exist to create the offer.
-      // And check the bch wallet fulfils the criteria for the p2wdb
-      await this.ensureFunds(offerEntity)
+      // Input Validation
+      const offerEntity = this.offerEntity.validate(entryObj);
+
+      // Ensure sufficient AVAX tokens exist to create the offer.
+      await this.ensureFunds(offerEntity);
+
+      // Ensure BCH wallet has finished initializing
+      await this.adapters.wallet.bchWallet.walletInfoPromise;
+
+      // Ensure sufficient funds in the BCH wallet to make a P2WDB write.
       await this.adapters.p2wdb.checkForSufficientFunds(
         this.adapters.wallet.bchWallet.walletInfo.privateKey
-      )
+      );
 
       // Move the tokens to holding address.
-      const addressInfo = await this.getAddress()
-      const utxoInfo = await this.moveTokens(offerEntity, addressInfo)
-      console.log('utxoInfo: ', utxoInfo)
+      const addressInfo = await this.getAddress();
+      const utxoInfo = await this.moveTokens(offerEntity, addressInfo);
+      console.log("utxoInfo: ", utxoInfo);
 
       // create partial tx with the token inputs and avax output
-      await this.adapters.wallet.bchWallet.bchjs.Util.sleep(3000)
+      await this.adapters.wallet.bchWallet.bchjs.Util.sleep(3000);
       const partialTx = await this.adapters.wallet.createPartialTxHex(
         offerEntity.rateInSats,
         addressInfo.privateKey
-      )
+      );
 
       // Update the offer with the new UTXO information and the partialTx information.
-      offerEntity.utxoTxid = utxoInfo.txid
-      offerEntity.utxoVout = utxoInfo.outputIdx
-      offerEntity.txHex = partialTx.txHex
-      offerEntity.addrReferences = partialTx.addrReferences
-      offerEntity.hdIndex = 3
+      offerEntity.utxoTxid = utxoInfo.txid;
+      offerEntity.utxoVout = utxoInfo.outputIdx;
+      offerEntity.txHex = partialTx.txHex;
+      offerEntity.addrReferences = partialTx.addrReferences;
+      offerEntity.hdIndex = 3;
 
       // Add offer to P2WDB.
       const hash = await this.adapters.p2wdb.write({
         wif: this.adapters.wallet.bchWallet.walletInfo.privateKey,
         data: offerEntity,
-        appId: 'swapTest555'
-      })
+        appId: "swapTest555"
+      });
       // console.log('hash: ', hash)
 
       // Update the UTXO store in both wallets.
-      await this.adapters.wallet.bchWallet.bchjs.Util.sleep(3000)
+      await this.adapters.wallet.bchWallet.bchjs.Util.sleep(3000);
       await Promise.all([
         this.adapters.wallet.avaxWallet.getUtxos(),
         this.adapters.wallet.bchWallet.getUtxos()
-      ])
+      ]);
 
-      return hash
+      return hash;
     } catch (err) {
-      wlogger.error('Error in use-cases/entry.js/createOffer())')
-      console.log(err)
-      throw err
+      wlogger.error("Error in use-cases/entry.js/createOffer())");
+      console.log(err);
+      throw err;
     }
   }
 
   // Move the tokens indicated in the offer to a temporary holding address.
   // This will generate the UTXO used in the Signal message. This function
   // moves the funds and returns the UTXO information.
-  async moveTokens (offerEntity, addressInfo) {
+  async moveTokens(offerEntity, addressInfo) {
     try {
-      console.log('addressInfo: ', addressInfo)
+      console.log("addressInfo: ", addressInfo);
 
       // Turn token into sats
-      const assets = this.adapters.wallet.avaxWallet.utxos.assets
-      const asset = assets.find(item => item.assetID === offerEntity.tokenId)
-      const amount = offerEntity.numTokens * Math.pow(10, asset.denomination)
+      const assets = this.adapters.wallet.avaxWallet.utxos.assets;
+      const asset = assets.find(item => item.assetID === offerEntity.tokenId);
+      const amount = offerEntity.numTokens * Math.pow(10, asset.denomination);
 
       const receiver = {
         address: addressInfo.address,
         amount,
         assetID: offerEntity.tokenId
-      }
+      };
 
-      const txid = await this.adapters.wallet.avaxWallet.send([receiver])
+      const txid = await this.adapters.wallet.avaxWallet.send([receiver]);
 
       const utxoInfo = {
         txid,
-        vout: '00000000' // equivalent to vout 0
-      }
+        vout: "00000000" // equivalent to vout 0
+      };
 
-      return utxoInfo
+      return utxoInfo;
     } catch (err) {
-      console.error('Error in moveTokens(): ', err)
-      throw err
+      console.error("Error in moveTokens(): ", err);
+      throw err;
     }
   }
 
   // Ensure that the wallet has enough AVAX and tokens to complete the trade.
-  async ensureFunds (offerEntity) {
+  async ensureFunds(offerEntity) {
     try {
       // Get Assets.
-      const assets = this.adapters.wallet.avaxWallet.utxos.assets
+      const assets = this.adapters.wallet.avaxWallet.utxos.assets;
 
       // Sell Offer
-      if (offerEntity.buyOrSell.includes('sell')) {
-        const asset = assets.find(item => item.assetID === offerEntity.tokenId)
+      if (offerEntity.buyOrSell.includes("sell")) {
+        const asset = assets.find(item => item.assetID === offerEntity.tokenId);
 
         // Turn token into sats
-        const denomination = asset?.denomination || 0
-        const amount = offerEntity.numTokens * Math.pow(10, denomination)
+        const denomination = asset?.denomination || 0;
+        const amount = offerEntity.numTokens * Math.pow(10, denomination);
         if (!asset || asset.amount < amount) {
           throw new Error(
-            'App wallet does not have enough tokens to satisfy the SELL offer.'
-          )
+            "App wallet does not have enough tokens to satisfy the SELL offer."
+          );
         }
       } else {
         // Buy Offer
       }
 
-      return true
+      return true;
     } catch (err) {
-      console.error('Error in ensureFunds()')
-      throw err
+      console.error("Error in ensureFunds()");
+      throw err;
     }
   }
 
   // Avax: retrieves a new key pair from the HD key ring as an object that contains a private key,
   // public address, and the index of the HD wallet that the key pair was generated from.
-  async getAddress (hdIndex) {
+  async getAddress(hdIndex) {
     try {
-      const keypair = await this.adapters.wallet.getAvaxKeyPair(hdIndex)
+      const keypair = await this.adapters.wallet.getAvaxKeyPair(hdIndex);
 
       return {
         address: keypair.getAddressString(),
         privateKey: keypair.getPrivateKeyString(),
         publicKey: keypair.getPublicKeyString(),
         hdIndex: keypair.hdIndex
-      }
+      };
     } catch (error) {
-      console.log(`Error on offer/getAddress(): ${error.message}`)
-      throw error
+      console.log(`Error on offer/getAddress(): ${error.message}`);
+      throw error;
     }
   }
 }
 
-module.exports = OfferLib
+module.exports = OfferLib;
