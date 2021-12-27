8e0235dcf89a93604766b12ce5f6a8002d7a79aa
Chris Troutner <chris.troutner@gmail.com>
2021-12-27 09:56:02 -0800

    fix(ipfs): Disabling IPFS node for now

--- test/e2e/manual/create-offer.js
+++ test/e2e/manual/create-offer.js
@@ -4,33 +4,33 @@
   Ensure the REST API is up an running before running this test.
 */
 
-const axios = require('axios')
+const axios = require("axios");
 
-const LOCALHOST = 'http://localhost:5700'
+const LOCALHOST = "http://localhost:5700";
 
-async function start () {
+async function start() {
   try {
     const mockOffer = {
-      lokadId: 'SWP',
+      lokadId: "SWP",
       messageType: 1,
       messageClass: 1,
-      tokenId: '2tEi6r6PZ9VXHogUmkCzvijmW81TRNjtKWnR4FA55zTPc87fxC',
-      buyOrSell: 'sell',
+      tokenId: "2aK8oMc5izZbmSsBiNzb6kPNjXeiQGPLUy1sFqoF3d9QEzi9si",
+      buyOrSell: "sell",
       rateInSats: 1000,
       minSatsToExchange: 10,
-      numTokens: 3
-    }
+      numTokens: 1
+    };
 
     const options = {
-      method: 'post',
+      method: "post",
       url: `${LOCALHOST}/offer`,
       data: { offer: mockOffer }
-    }
+    };
 
-    const result = await axios(options)
-    console.log('result.data: ', result.data)
+    const result = await axios(options);
+    console.log("result.data: ", result.data);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
-start()
+start();
