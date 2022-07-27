0d5767fa745fa113b94d84d1dcd85752c5d55aa0
Chris Troutner <chris.troutner@gmail.com>
2022-07-26 18:13:19 -0700

    Added script to sweep AVAX wallet

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -58,7 +58,7 @@ class Adapters {
       // Open the wallet files
       const bchWallet = await this.wallet.openWallet(false)
       const avaxWallet = await this.wallet.openWallet(true)
-      // console.log('walletData: ', walletData)
+      console.log('avaxWallet: ', avaxWallet)
 
       this.p2wdb = new P2wdbAdapter({ bchjs: this.bchjs })
 
